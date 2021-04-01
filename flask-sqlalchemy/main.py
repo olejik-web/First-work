4f9d991c1cc339abed77ad15cc0a1ed7b7db4dfc
olegator2004 <59178029+olejik-web@users.noreply.github.com>
2021-04-01 19:21:07 +0300

    Add files via upload

---
+++ flask-sqlalchemy/main.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from flask import Flask
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/mars.db")
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.start_date = datetime.now()
+    job.is_finished = False
+    db_sess = db_session.create_session()
+    db_sess.add(job)
+    db_sess.commit()
+    # app.run()
+
+
+if __name__ == '__main__':
+    main()
